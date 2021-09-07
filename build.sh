a314ed342324dc6b275b78e82601a8b23c8977a3
John Spurlock <john@skymethod.com>
2021-09-07 12:17:04 -0500

    Fix build, verbose at the end.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 DENO_VERSION="v1.13.2"
 DENOFLARE_VERSION="fd1e719b797aa1b51cfea8b44a77686cde5bd046"
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-$DENO_VERSION sh -s $DENO_VERSION
-NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --unstable --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts site generate --verbose . ./output
+NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --unstable --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts site generate . ./output --verbose
