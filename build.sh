aa8e7364ff712738ddc84abbcb2298d486feeb4c
John Spurlock <john@skymethod.com>
2021-09-12 13:37:31 -0500

    Update to latest df.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DENO_VERSION="v1.13.2"
-DENOFLARE_VERSION="5c18e8976260d1ee09656ffc6639102718d4fd09"
+DENOFLARE_VERSION="0708494a16aaa00db5d6ca9dc476a9ce649a8011"
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-$DENO_VERSION sh -s $DENO_VERSION
 NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --unstable --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts site generate . ./output --verbose
