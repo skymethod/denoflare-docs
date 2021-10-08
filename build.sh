2b25f3a41d90835457c1913ef0ee3cab882f66d1
John Spurlock <john@skymethod.com>
2021-10-08 17:01:27 -0500

    Add Algolia DocSearch config, point to new df.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DENO_VERSION="v1.14.3"
-DENOFLARE_VERSION="10f9fdac6d1be6456fb67f04be37b9d605e0a157"
+DENOFLARE_VERSION="0454bceedf13e302bb9d9fad336cee61c06c4af0"
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-$DENO_VERSION sh -s $DENO_VERSION
 NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --unstable --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts site generate . ./output --verbose
